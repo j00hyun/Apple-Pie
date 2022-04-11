767658b1ae9d5bc2bb68fc37de3c2baf7b98920d
j00hyun <loove1997@naver.com>
2022-04-11 21:22:52 +0900

    기본 기능 완료

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Park JooHyun on 2022/04/11.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
