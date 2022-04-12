3d002e70d3d89de8c7a4586bf3a3461292aced1e
j00hyun <loove1997@naver.com>
2022-04-12 16:54:32 +0900

    주석추가

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -8,17 +8,18 @@
 import Foundation
 
 struct Game {
-    var word: String
-    var incorrectMovesRemaining: Int
-    var guessedLetters: [Character]
+    var word: String // 현재 라운드 단어
+    var incorrectMovesRemaining: Int // 남은 기회 수
+    var guessedLetters: [Character] // 플레이어가 선택한 알파벳 기록
     
+    // 단어에서 현재까지 맞춘 알파벳 오픈
     var formattedWord: String {
         var guessedWord = ""
         
         for letter in word {
-            if guessedLetters.contains(letter) {
+            if guessedLetters.contains(letter) { // 맞춘 알파벳 오픈
                 guessedWord += "\(letter)"
-            } else {
+            } else { // 나머지 알파벳 _ 처리
                 guessedWord += "_"
             }
         }
@@ -26,9 +27,11 @@ struct Game {
         return guessedWord
     }
     
+    // 플레이어가 선택한 알파벳이 단어에 속해있는지 확인
     mutating func playerGuessed(letter: Character) -> Bool {
         guessedLetters.append(letter)
         
+        // 속해있지 않을 경우 기회 1 감소
         if !word.contains(letter) {
             incorrectMovesRemaining -= 1
             return false
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
-    let incorrectMovesAllowed = 7
-    var totalWins = 0 {
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"] // 게임 단어 목록
+    let incorrectMovesAllowed = 7 // 남은 기회
+    var totalWins = 0 { // 맞춘 단어 수
         didSet {
             newRound()
         }
     }
-    var totalLosses = 0 {
+    var totalLosses = 0 { // 틀린 단어 수
         didSet {
             newRound()
         }
     }
-    var totalScores = [0, 0]
+    var totalScores = [0, 0] // 플레이어 별 점수
     var currentGame: Game!
-    var currPlayer = 0
+    var currPlayer = 0 // 현재 차례 플레이어
     
     @IBOutlet var treeImageView: UIImageView!
     @IBOutlet var correctWordLabel: UILabel!
@@ -39,13 +39,18 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        // 현재 단어
         let letters = currentGame.formattedWord.map { String($0) }
         let wordWithSpacing = letters.joined(separator: " ")
         correctWordLabel.text = wordWithSpacing
         
+        // 맞춘 단어, 틀린 단어 수
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        
+        // 사과나무 이미지
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
                 
+        // 플레이어
         for (i, player) in players.enumerated() {
             player.text = "Score: \(totalScores[i])"
             player.backgroundColor = .clear
@@ -54,48 +59,53 @@ class ViewController: UIViewController {
         players[currPlayer].backgroundColor = .yellow
     }
     
+    // 새로운 라운드 세팅
     func newRound() {
         if !listOfWords.isEmpty {
             let newWord = listOfWords.removeFirst()
             currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
             enableLetterButtons(true)
-        } else {
+        } else { // 게임 종료
             enableLetterButtons(false)
         }
         updateUI()
     }
     
+    // 플레이어가 알파벳 버튼을 눌렀을 경우
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.configuration!.title!
         let letter = Character(letterString.lowercased())
         
-        if currentGame.playerGuessed(letter: letter) {
+        if currentGame.playerGuessed(letter: letter) { // 올바른 알파벳을 선택한 경우 점수 상승
             totalScores[currPlayer] += 1
-        } else {
+        } else { // 틀렸을 경우 플레이어 차례 변경
             turnPlayer()
         }
         
         updateGameState()
     }
     
+    // 전체 알파벳 버튼 활성화 or 비활성화
     func enableLetterButtons(_ enable: Bool) {
         for button in letterButtons {
             button.isEnabled = enable
         }
     }
     
+    // 현재 라운드 상태 파악
     func updateGameState() {
-        if currentGame.incorrectMovesRemaining == 0 {
+        if currentGame.incorrectMovesRemaining == 0 { // 남은 기회가 없을 경우 다음 라운드 진행
             totalLosses += 1
-        } else if currentGame.word == currentGame.formattedWord {
+        } else if currentGame.word == currentGame.formattedWord { // 단어를 맞췄을 경우 맞춘 플레이어 점수 상승, 다음 라운드 진행
             totalScores[currPlayer] += currentGame.incorrectMovesRemaining
             totalWins += 1
-        } else {
+        } else { // 현재 라운드 계속 진행
             updateUI()
         }
     }
     
+    // 플레이어 차례 변경
     func turnPlayer() {
         currPlayer += 1
         
